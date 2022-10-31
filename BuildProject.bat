3deee6920816fb0985b613f57060679c7b786c88
ZhuElly <34325707+Elly2018@users.noreply.github.com>
2022-10-31 11:50:11 +0800

    Update batch file

--- BuildProject.bat
+++ BuildProject.bat
@@ -5,7 +5,7 @@ rmdir obj /s /q
 
 set DEV_PREFIX=c:%HOMEPATH%\Documents\project_unity_development
 
-docfx metadata %DEV_PREFIX%\Funique.csproj %DEV_PREFIX%\Funique.Cinema.csproj  %DEV_PREFIX%\Funique.Client.csproj %DEV_PREFIX%\Funique.Core.csproj %DEV_PREFIX%\Funique.Designer.csproj %DEV_PREFIX%\Funique.Media.csproj %DEV_PREFIX%\Funique.Network.csproj %DEV_PREFIX%\Funique.Player.csproj %DEV_PREFIX%\Funique.Setting.csproj %DEV_PREFIX%\Funique.Streaming.csproj %DEV_PREFIX%\Funique.UI.csproj %DEV_PREFIX%\Funique.Verify.csproj %DEV_PREFIX%\Funique.Video.csproj --filter filter.yml
+docfx metadata %DEV_PREFIX%\Funique.Cinema.csproj  %DEV_PREFIX%\Funique.Client.csproj %DEV_PREFIX%\Funique.Core.csproj %DEV_PREFIX%\Funique.Designer.csproj %DEV_PREFIX%\Funique.Media.csproj %DEV_PREFIX%\Funique.Network.csproj %DEV_PREFIX%\Funique.Player.csproj %DEV_PREFIX%\Funique.Setting.csproj %DEV_PREFIX%\Funique.Streaming.csproj %DEV_PREFIX%\Funique.UI.csproj %DEV_PREFIX%\Funique.Verify.csproj %DEV_PREFIX%\Funique.Video.csproj --filter filter.yml
 ren _api api
 docfx build
 pause

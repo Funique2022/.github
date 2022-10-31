a01e2b683156f754edcf54be314bb2a7c33c11df
ZhuElly <34325707+Elly2018@users.noreply.github.com>
2022-10-31 11:47:30 +0800

    Update bat

--- BuildProject.bat
+++ BuildProject.bat
@@ -3,10 +3,9 @@ rmdir api /s /q
 rmdir _api /s /q
 rmdir obj /s /q
 
-set SERVER_PREFIX=c:%HOMEPATH%\Documents\funique_master_server\project_master_server
-set CLIENT_PREFIX=c:%HOMEPATH%\Documents\DaVinci_2022
+set DEV_PREFIX=c:%HOMEPATH%\Documents\project_unity_development
 
-docfx metadata %SERVER_PREFIX%\Funique.csproj %SERVER_PREFIX%\Funique.Cinema.csproj %CLIENT_PREFIX%\Funique.Core.csproj %SERVER_PREFIX%\Funique.Editor.csproj %SERVER_PREFIX%\Funique.Media.csproj %SERVER_PREFIX%\Funique.Player.csproj %SERVER_PREFIX%\Funique.Network.csproj %SERVER_PREFIX%\Funique.Setting.csproj %SERVER_PREFIX%\Funique.Streaming.csproj %SERVER_PREFIX%\Funique.UI.csproj %CLIENT_PREFIX%\Funique.Client.csproj %CLIENT_PREFIX%\Funique.Dissonance.csproj %SERVER_PREFIX%\Funique.Video.csproj --filter filter.yml
+docfx metadata %DEV_PREFIX%\Funique.csproj %DEV_PREFIX%\Funique.Cinema.csproj  %DEV_PREFIX%\Funique.Client.csproj %DEV_PREFIX%\Funique.Core.csproj %DEV_PREFIX%\Funique.Designer.csproj %DEV_PREFIX%\Funique.Media.csproj %DEV_PREFIX%\Funique.Network.csproj %DEV_PREFIX%\Funique.Player.csproj %DEV_PREFIX%\Funique.Setting.csproj %DEV_PREFIX%\Funique.Streaming.csproj %DEV_PREFIX%\Funique.UI.csproj %DEV_PREFIX%\Funique.Verify.csproj %DEV_PREFIX%\Funique.Video.csproj --filter filter.yml
 ren _api api
 docfx build
 pause

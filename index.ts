126f259633312f5ee8e1f12699e4bd2408b8f0df
ZhuElly <34325707+Elly2018@users.noreply.github.com>
2023-04-26 11:52:14 +0800

    Update

--- index.ts
+++ index.ts
@@ -3,6 +3,7 @@ import axios from 'axios'
 
 const port = process.env.port || 80;
 const backend_url = process.env.backend || "http://127.0.0.1:15755";
+const token = process.env.token;
 var config:any | undefined = undefined
 const default_data = {
     
@@ -10,7 +11,7 @@ const default_data = {
 
 const app = exp();
 
-app.use(exp.static('/static'));
+app.use(exp.static('static'));
 
 app.listen(port, () => {
     console.log(`Start listening to port: ${port}`)
